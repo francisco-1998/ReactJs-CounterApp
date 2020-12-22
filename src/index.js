560c7330131a5020ed29b9a86c5a90a35cb35f81
francisco-1998 <francmarinc@gmail.com>
2020-12-22 14:15:24 -0500

    Practica de lo aprendido, creando mi propia componente

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from 'react-dom'
-import PrimeraApp from "./PrimeraApp";
+// import PrimeraApp from "./PrimeraApp";
 import './index.css';
+import CounterApp from "./CounterApp";
 
 const divRoot = document.querySelector('#root');
 
 
-ReactDOM.render(<PrimeraApp  saludo = 'Francisco'/>, divRoot)
+// ReactDOM.render(<PrimeraApp  saludo = 'Francisco'/>, divRoot)
+ReactDOM.render(<CounterApp value = { 1 } />, divRoot)
+
+
+
