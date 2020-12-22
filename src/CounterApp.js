d62d61cf737eae70c3e5a727dd23f490a94023eb
francisco-1998 <francmarinc@gmail.com>
2020-12-22 14:34:30 -0500

    Finish el diseño de mi app counter

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -6,7 +6,10 @@ import PropTypes from 'prop-types';
 const CounterApp = ({ value }) => {
     return <Fragment>
         <h1>Counter App</h1>
-        <h2>{ value }</h2>
+        <h2>{value}</h2>
+        <button class="btn">
+            +1
+        </button>
     </Fragment>
 }
 
