2a2de044bf035daf6acc4eecfc5f0a831cf81c03
francisco-1998 <francmarinc@gmail.com>
2020-12-22 12:09:34 -0500

    Como mostrar varios elementos en el DOM, usando un div, fragment, y </>

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 //Functional component
-const PrimeraApp = ()=>{
-    return <h1>Hola Francisco</h1>
+const PrimeraApp = () => {
+    return (
+        <>
+            <h1>Hola Francisco</h1>
+            <h3>Saludos!!</h3>
+        </>
+    );
 }
 
 
